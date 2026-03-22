74a5b8963992571c31828817d5f2cea9b74d4bee
Михаил Казаков <mishakazakov82@gmail.com>
2026-03-22 15:24:27 +0500

    Разработанные общие классы. Осенников

---
+++ Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ChatStudents_Kazakov.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using (var Stream = new MemoryStream(imageData))
+            {
+                Stream.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.StreamSource = Stream;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
---
+++ Classes/Common/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatStudents_Kazakov.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=student.permaviat.ru;Trusted_Connection=False;Database=;User=Pwd=;";
+    }
+}
