cf331ae20c657db3befe75495716073a5d6cc377
Михаил Казаков <mishakazakov82@gmail.com>
2026-03-22 16:10:07 +0500

    Разработанный пользовательский элемент Message. Осенников

---
+++ Pages/Items/Message.xaml.cs
@@ -0,0 +1,33 @@
+﻿using ChatStudents_Kazakov.Classes.Common;
+using ChatStudents_Kazakov.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ChatStudents_Kazakov.Pages.Items
+{
+    /// <summary>
+    /// Логика взаимодействия для Message.xaml
+    /// </summary>
+    public partial class Message : UserControl
+    {
+        public Message(Messages Messages, Users UserFrom)
+        {
+            InitializeComponent();
+            imgUser.Source = BitmapFromArrayByte.LoadImage(UserFrom.Photo);
+            FIO.Content = UserFrom.ToFIO();
+            tbMessage.Text = Messages.Message;
+        }
+    }
+}
